89cff96fb0967e155cbe3f20681b1e23e5b6eb10
Manuel Herrmann <0@0x17.de>
2017-04-05 22:15:00 +0200

    setup/add user flow

---
+++ code/add-user.sh
@@ -0,0 +1,5 @@
+%> cd ${HARPOON_RUN_DIRECTORY}
+%> Harpoon --genuser --save
+Username: 
+Password: 
+users.ini updated.
---
+++ code/setup-user.sh
@@ -0,0 +1,13 @@
+%> cd ${HARPOON_RUN_DIRECTORY}
+%> Harpoon --setup --save
+Login database type (dummy/ini) [ini]: 
+Backlog database type (none/postgres) [postgres]: 
+Enable IRC service (y/n) [y]: 
+IRC settings database type (dummy/ini) [ini]: 
+Collect IRC backlog (y/n) [y]: 
+Enable WebChat (y/n) [y]: 
+
+The first can now be created
+Username: 
+Password: 
+users.ini updated.
